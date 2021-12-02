fcaf2f874cfb7b159e82fe60aa75767d6e850e03
sujivan96 <suji96.info@gmail.com>
2021-12-02 20:25:08 +0530

    prevent entering spamming entries.

--- src/components/Forms.js
+++ src/components/Forms.js
@@ -1,78 +1,100 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 import { Form, Button } from 'react-bootstrap'
 import axios, { Axios } from 'axios';
+import swal from 'sweetalert'
 
 export default function Forms() {
 
-const url = "http://localhost:4000/create"
-const [data, setData] = useState({
-  country: "",
-  cases : "",
-  deaths : "",
-  date : "",
-  email : ""
-})
-
-function submit(e){
-e.preventDefault();
-axios.post(url, {
-  country: data.country,
-  cases : data.cases,
-  deaths : data.deaths,
-  date : data.date,
-  email : data.email
-})
-
-}
-
-function handle(e){
-const newData = {...data}
-newData[e.target.id] = e.target.value
-setData(newData)
-console.log(newData)
-}
-
-
-
-    return (
-        <div className="coniner">
-        <div className="formbox">
-           <Form  onSubmit={(e) => submit(e)}>
-               <h1>Covide 19 data form</h1>
-  <Form.Group className="mb-3">
-    <Form.Label>Country</Form.Label>
-    <Form.Control onChange = {(e)=> handle(e)} id="country" value={data.country} type="text" placeholder="Enter Your Country" />
-    
-  </Form.Group>
-
-  <Form.Group className="mb-3">
-    <Form.Label>Covid Cases</Form.Label>
-    <Form.Control onChange = {(e)=> handle(e)} id="cases" value={data.cases}  type="number" placeholder="Number of Covid Cases" />
-  </Form.Group>
-
-  <Form.Group className="mb-3">
-    <Form.Label>Covid Deaths</Form.Label>
-    <Form.Control onChange = {(e)=> handle(e)} id="deaths" value={data.deaths}  type="number" placeholder="Number of Covid Deaths" />
-  </Form.Group>
-
-  <Form.Group className="mb-3">
-    <Form.Label>Date of Entry</Form.Label>
-    <Form.Control onChange = {(e)=> handle(e)} id="date" value={data.date}  type="Date" placeholder="Number of Covid Deaths" />
-  </Form.Group>
-
-
-  <Form.Group className="mb-3" controlId="formBasicEmail">
-    <Form.Label>Email address</Form.Label>
-    <Form.Control onChange = {(e)=> handle(e)} id="email" value={data.email}  type="email" placeholder="Enter email" />
-  </Form.Group> 
+  
+  const url = "http://localhost:4000/create"
+  let btnRef = useRef()
+  const [data, setData] = useState({
+    country: "",
+    cases: "",
+    deaths: "",
+    date: "",
+    email: ""
+  })
 
   
-  
-  <Button variant="primary" type="submit">
-    Submit
-  </Button>
-  <br/><br/><br/><br/>
-</Form>
-        </div></div>
-    )
+
+  function submit(e) {
+    e.preventDefault();
+
+    if(btnRef.current){
+      btnRef.current.setAttribute("disabled", "disabled");
+    }
+   
+    axios.post(url, {
+      country: data.country,
+      cases: data.cases,
+      deaths: data.deaths,
+      date: data.date,
+      email: data.email
+    })
+      .then(res => {
+        swal("Data Inserted", "Thank You", "success");
+      })
+      .catch(err => {
+        swal("Error", "please try again", "error");
+      })
+
+
+  }
+
+  function handle(e) {
+    const newData = { ...data }
+    newData[e.target.id] = e.target.value
+    setData(newData)
+    console.log(newData)
+  }
+
+
+
+  return (
+    <div className="coniner">
+      <div className="formbox">
+        <Form onSubmit={(e) => submit(e)}>
+          <h1>Covide 19 data form</h1>
+          
+          <Form.Group className="mb-3">
+            <Form.Label>Country</Form.Label>
+            <Form.Control onChange={(e) => handle(e)} id="country" value={data.country} type="text" placeholder="Enter Your Country" required />
+
+          </Form.Group>
+
+          <Form.Group className="mb-3">
+            <Form.Label>Covid Cases</Form.Label>
+            <Form.Control onChange={(e) => handle(e)} id="cases" value={data.cases} type="number" placeholder="Number of Covid Cases" required />
+          </Form.Group>
+
+          <Form.Group className="mb-3">
+            <Form.Label>Covid Deaths</Form.Label>
+            <Form.Control onChange={(e) => handle(e)} id="deaths" value={data.deaths} type="number" placeholder="Number of Covid Deaths" required />
+          </Form.Group>
+
+          <Form.Group className="mb-3">
+            <Form.Label>Date of Entry</Form.Label>
+            <Form.Control onChange={(e) => handle(e)} id="date" value={data.date} type="Date" placeholder="Number of Covid Deaths" required />
+          </Form.Group>
+
+
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control onChange={(e) => handle(e)} id="email" value={data.email} type="email" placeholder="Enter email" required />
+          </Form.Group>
+
+         
+
+
+
+          <Button ref={btnRef}   type="submit" >
+            Submit
+          </Button>
+          
+          <br /><br /><br /><br />
+        </Form>
+
+      </div></div>
+  )
 }
